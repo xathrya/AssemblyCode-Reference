8f948e635e17e4ac7e84515bfba3ec3045a044af
xathrya <me@xathrya.id>
2018-07-13 19:22:48 +0700

    Rewrite 1.declaration, remove relocation.asm

---
+++ Code/x86/NASM/src/01.declaration/2.reserve-space.asm
@@ -0,0 +1,48 @@
+; reserve-space.asm
+;
+; Reserve space for uninitialized data in .bss section.
+;
+; Compile:
+;   (linux)
+;   $ nasm -f elf32 -o reserve-space.o reserve-space.asm
+;
+;   (win32)
+;   $ nasm -f win32 -o reserve-space.o reserve-space.asm
+;
+; Linking:
+;   (linux)
+;   $ ld -m elf_i386 -o reserve-space reserve-space.o
+;
+;   (windows)
+;   $ ld -m i386pe -o reserve-space reserve-space.o
+;
+; This code snippet is for examples to topics
+; and should not be run as an executable.
+; 
+;-----------------------------------------------------------------------------
+
+    global _start
+
+; Reserve some space in memory
+; 
+; When generating the binary code, NASM will create a stream of bytes in the
+; corresponding setion. This stream of data is unintialized or filled with zeroes.
+;
+; Labelling is not a requirement, but if data is going to be  referred then
+; labelling is a must.
+;
+; Reserving space is done by the resX instruction where X is the data type.
+;   resb = write bytes (8-bit)
+;   resw = write words (16-bits)
+;   resd = write double words (32-bits)
+;   resq = write quad words (64-bits) or double-precision float
+;   rest = write 80-bits floating-point constant (extended-precision float)
+
+section .bss
+    bbuffer:    resb    5       ; reserve 5 bytes space 
+    wbuffer:    resw    1       ; reserve a word
+    qbuffer:    resd    10      ; array of qword
+
+section .text
+_start:
+    hlt         ; Halt the execution
---
+++ Code/x86/NASM/src/01.declaration/3.constants.asm
@@ -0,0 +1,64 @@
+; constants.asm
+;
+; creating a constants value.
+;
+; Compile:
+;   (linux)
+;   $ nasm -f elf32 -o constants.o constants.asm
+;
+;   (win32)
+;   $ nasm -f win32 -o constants.o constants.asm
+;
+; Linking:
+;   (linux)
+;   $ ld -m elf_i386 -o define-data define-data.o
+;
+;   (windows)
+;   $ ld -m i386pe -o define-data define-data.o
+;
+; This code snippet is for examples to topics
+; and should not be run as an executable.
+; 
+;-----------------------------------------------------------------------------
+
+    global _start
+
+; A constant can be declared by using 'equ' (equate) instruction. There would
+; be no space allocation in the final code. The symbol or token is simple 
+; will be evaluate later and any occurrence of it will be replaced by corresponding value.
+;
+; Therefore, defining a constant would simply sets it to be the value.
+
+lenequ:  equ 4      ; whenever `lenequ` is found in source code
+                    ; it will be replace with 4
+
+; The equ is used similar to what #define did in C
+; #define lenequ 4
+
+; Another equivalent instruction for declaring a constant is using %define
+; The %define instruction can be placed anywhere, it could be even placed
+; inside a scope of function.
+
+%define DLENEQU 10
+
+section .data
+; Lets compare it to the 'db'
+    lendb:   db 4   ; reserve 1 byte of memory 
+
+section .text
+_start:
+; to illustrate the memory layout, we will use following code
+    mov     eax, lenequ
+    mov     eax, dword [lendb]
+    
+; If we illustrate the memory layout, it would be something like this
+; (assuming that the address of code is directly after the data section)
+;
+; addr      code            label   instruction
+;-------------------------------------------
+; 402000                    lenequ  equ 4
+; 402000    04              lendb   db  4
+; 402001    b8 04 00 00 00          mov eax, lenequ
+; 402006    a1 00 20 40 00          mov eax, [lendb]
+
+    hlt         ; Halt the execution
